628cf0e513a136e3e551a727468834ade2f0d714
benodeveloper <listoun.developer@gmail.com>
2025-03-08 22:18:56 +0000

    sancks.image

--- lua/plugins/numb.lua
+++ lua/plugins/numb.lua
@@ -2,7 +2,10 @@ return {
     {
         "nacro90/numb.nvim",
         config = function()
-            require("numb").setup()
+            require("numb").setup({
+                show_numbers = true, -- Enable 'number' for the window while peeking
+                show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+            })
         end,
     },
 }
