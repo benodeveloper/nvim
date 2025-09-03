2645872dc10950ebe8e35b168e6b8d98fe2e952f
benodeveloper <benodeveloper@gmail.com>
2025-09-04 00:36:08 +0100

    last update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,9 @@
+local tests = require("utils.tests")
+
 -- Move highlighted lines up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-local tests = require("utils.tests")
 
--- Run tests for the current file
 vim.keymap.set("n", "<leader>tf", function()
     tests.run_gradle_test("file")
 end, { desc = "Run [T]est [F]ile" })
@@ -27,3 +27,27 @@ end, { desc = "Save code screenshot as file" })
 vim.keymap.set("v", "<leader>ss", function()
     require("nvim-silicon").shoot()
 end, { desc = "Create code screenshot" })
+
+-- nvim-dap
+vim.keymap.set(
+    "n",
+    "<leader>dB",
+    '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
+    { desc = "Add Conditional Breakpoint" }
+)
+vim.keymap.set("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "Dap Continue" })
+vim.keymap.set("n", "<leader>dsi", "<cmd>DapStepInto<CR>", { desc = "Dap Step Into" })
+vim.keymap.set("n", "<leader>dso", "<cmd>DapStepOver<CR>", { desc = "Dap Step Over" })
+vim.keymap.set("n", "<leader>dst", "<cmd>DapStepOut<CR>", { desc = "Dap Step Out" })
+vim.keymap.set("n", "<leader>dt", "<cmd>DapTerminate<CR>", { desc = "Dap Terminate" })
+vim.keymap.set("n", "<leader>dl", "<cmd>DapShowLog<CR>", { desc = "Dap Show Log" })
+vim.keymap.set("n", "<leader>dr", "<cmd>DapToggleRepl<CR>", { desc = "Dap Toggle Repl" })
+
+-- noice.nvim
+vim.keymap.set("n", "<space>nn", "<cmd>Noice<cr>", { desc = "Noice Messages" })
+vim.keymap.set("n", "<space>na", "<cmd>NoiceAll<cr>", { desc = "Noice All Messages" })
+vim.keymap.set("n", "<space>nl", "<cmd>NoiceLast<cr>", { desc = "Noice Last" })
+vim.keymap.set("n", "<space>ne", "<cmd>NoiceErrors<cr>", { desc = "Noice Errors" })
+vim.keymap.set("n", "<space>ns", function()
+    require("noice").redirect("Notifications")
+end, { desc = "Noice Notifications" })
--- lua/plugins/nvim-preview-svg.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nmassardot/nvim-preview-svg",
-    opts = {
-
-        browser = "Chrome", -- it can be any browser you want!
-        args = false, -- macOS versions newer than BigSur may not work with --args
-    },
-}
